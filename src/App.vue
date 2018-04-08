31ca725e1712a4051a19e73e69f0a7e1d46ef7b2
jeanboy <caojianbo@fotoable.com>
2018-04-08 14:52:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+
+
+    <div class="btn-group" role="group" aria-label="...">  
+      <button type="button" class="btn btn-default">Left</button>  
+      <button type="button" class="btn btn-default">Middle</button>  
+      <button type="button" class="btn btn-default">Right</button>  
+    </div>  
+  </div>
+</template>
+
+<script>
+// $(function() {
+//   alert(123);
+// });
+
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
